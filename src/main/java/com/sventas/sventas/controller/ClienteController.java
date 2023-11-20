d860a33b5c91193574dcc084c7b52bd63fb8a189
devpfan <128647947+devpfan@users.noreply.github.com>
2023-11-20 14:09:42 -0500

    operaciones controlador

--- src/main/java/com/sventas/sventas/controller/ClienteController.java
+++ src/main/java/com/sventas/sventas/controller/ClienteController.java
@@ -1,9 +1,41 @@
 package com.sventas.sventas.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sventas.sventas.model.Cliente;
+import com.sventas.sventas.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public List<Cliente> findAll(){
+        return clienteService.findAll();
+    }
+
+    @PostMapping
+    public Cliente create(@RequestBody Cliente cliente){
+        return clienteService.create(cliente);
+    }
+
+    @PutMapping
+    public Cliente update(@RequestBody Cliente cliente){
+        return clienteService.update(cliente);
+    }
+
+    @GetMapping("/{id}")
+    public Cliente findByid(@PathVariable("id") Integer idCliente){
+        return clienteService.findById(idCliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer idCliente){
+        clienteService.delete(idCliente);
+    }
 }
